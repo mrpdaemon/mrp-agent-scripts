87c1f748b9ca7a0a83f02b4f2e63c419b363358e
Mark Pariente <markp@augmentcode.com>
2026-04-24 20:40:05 +0000

    Collapse lstt into lst based on MRP_TASK
    If MRP_TASK is set, lst lists the current task's directory contents (previous lstt behavior). Otherwise it falls back to the original behavior of listing MRP_TASKS_DIR (or a specific task dir when an argument is given). Also guards against MRP_TASKS_DIR being unset.

--- init.sh
+++ init.sh
@@ -14,16 +14,19 @@ export PATH="$_MRP_SCRIPTS_DIR/bin:$PATH"
 
 alias gl='glow -p -w 120'
 lst() {
-  if [[ $# -ge 1 ]]; then
+  if [[ -z "${MRP_TASKS_DIR:-}" ]]; then
+    echo "MRP_TASKS_DIR is not set." >&2
+    return 1
+  fi
+
+  if [[ -n "$MRP_TASK" ]]; then
+    echo "Task directory contents for $MRP_TASK:"
+    ls "$MRP_TASKS_DIR/$MRP_TASK/"
+  elif [[ $# -ge 1 ]]; then
     ls "$MRP_TASKS_DIR/$1/"
   else
     ls "$MRP_TASKS_DIR"
   fi
-}
-
-lstt() {
-  echo "Task directory contents for $MRP_TASK:"
-  ls "$MRP_TASKS_DIR/$MRP_TASK/"
 }
 
 glt() {
